00b1c13040a1da454a24db5671c0971d8a69e81b
Rubén Medellín <chubas7@gmail.com>
2013-11-04 23:16:39 -0600

    Move program logic to game, think of various Engines

--- dmt/game.js
+++ dmt/game.js
@@ -1,7 +1,7 @@
 Class('Game')({
     prototype : {
         init : function(code) {
-            this._engine = new Engine(code);
+            this._engine = new Engine('2|1E3333-060A1E1F|31211-00021320');
         },
 
         run : function(code) {
